5e7af02cf92a9928c4f9b081d1a8b00256b82056
Vintets <vintets@mail.ru>
2021-02-02 18:35:49 +0300

    detach db models

--- cm_database.py
+++ cm_database.py
@@ -2,89 +2,55 @@
 # -*- coding: utf-8 -*-
 
 from datetime import datetime
-from sqlalchemy import create_engine, MetaData
+from sqlalchemy import create_engine
 from sqlalchemy.engine.url import URL
 from sqlalchemy import Table, Column, Integer, String, Text, DateTime, ForeignKey
-from sqlalchemy.ext.declarative import declarative_base
+# from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.sql.functions import current_timestamp
-# from sqlalchemy.dialects.mysql import INTEGER, DATETIME
-# from sqlalchemy.dialects.mysql import MEDIUMTEXT
-# UnsignedInt = db.Integer()
-# UnsignedInt = UnsignedInt.with_variant(INTEGER(unsigned=True), 'mysql')
-from configs.config import DBNAME, DATABASE
+from sqlalchemy.orm import sessionmaker
+from configs.config import DATABASE
+from cm_db_models import Base, Partitions, Elements, Users, LogRequests, create_db
 
 
-Base = declarative_base()
+# Base = declarative_base()
 
 
-class BaseModel(Base):
-    __abstract__ = True
-
-    id = Column(Integer, nullable=False, unique=True, primary_key=True, autoincrement=True)
-    # created_at = Column(TIMESTAMP, nullable=False)
-    # updated_at = Column(TIMESTAMP, nullable=False)
-
-    def __repr__(self):
-        return '<{0.__class__.__name__}(id={0.id!r})>'.format(self)
-
-
-class Partitions(BaseModel):
-    __tablename__ = 'partitions'
-    header = Column(String(255), nullable=False)
-    visible = Column(Integer, nullable=False, default=1, server_default=1)
-    parent = Column(Integer, nullable=False, default=0, server_default=0)
-    description = Column(Text)
-
-
-class Elements(BaseModel):
-    __tablename__ = 'elements'
-    header = Column(String(255), nullable=False)
-    visible = Column(Integer, nullable=False, default='1', server_default='1')
-    # parent_id = Column(Integer, nullable=False, default='0', server_default='0', index=True)
-    parent_id = Column(Integer, ForeignKey('partitions.id', ondelete='CASCADE'), nullable=False, default='0', server_default='0', index=True)
-    description = Column(Text)
-    syntax = Column(String(255), nullable=False)
-    parameters = Column(Text)
-    example = Column(Text)
-    notes = Column(Text)
-    keywords = Column(String(255), index=True)
-    version_cm_major = Column(Integer, nullable=False, default='4', server_default='4')
-    version_cm_minor = Column(Integer, nullable=False, default='13', server_default='13')
-    version_cm_build = Column(Integer, nullable=False, default='0', server_default='0')
-    version_cm_releaselevel = Column(String(16), nullable=True, default=None, server_default=None)
-
-    # def __unicode__(self):
-        # return self.header
-
-    # def __repr__(self):
-        # return 'Element: {}'.format(self.header)
-
-
-class Users(BaseModel):
-    __tablename__ = 'users'
-    chat_id = Column(String(32), nullable=False, unique=True)
-    # created = Column(TIMESTAMP, nullable=False)
-    created = Column(DateTime, nullable=False, default=current_timestamp(), server_default='2000-01-01 00:00:00')
-    last_update = Column(DateTime, nullable=False, default=datetime(2000, 1, 1, 0, 0, 0), server_default='2000-01-01 00:00:00')
-
-
-class LogRequests(BaseModel):
-    __tablename__ = 'log_requests'
-    user_id = Column(Integer, ForeignKey('users.id', ondelete='CASCADE'), nullable=False)
-    request = Column(String(64), nullable=False)
-    rtime = Column(DateTime, nullable=False, default=current_timestamp(), server_default='2000-01-01 00:00:00')
+def filling_db(session):
+    new_partition = Partitions(name='Введение')
+    session.add(new_partition)
+    new_element = Elements(
+                    header='математические операции',
+                    parent_id=1,
+                    description=(
+                                'Операция                Приоритет Пример\n'
+                                'Скобки приоритета "( )" 1         $var = (1 + 2) * 2\n'
+                                'Умножение "*"           2         $var = 1 * 2\n'
+                                'Деление "/"             2         $var = 1 / 2\n'
+                                'Сложение "+"            3         $var = 1 + 2\n'
+                                'Вычитание "-"           3         $var = 1 - 2\n'
+                                ),
+                    keywords='математические операции, математика, скобки, умножение, деление, сложение, вычитание, скобка, плюс, минус, умножить, разделить',
+                    version_cm_major=3,
+                    version_cm_minor=2,
+                    version_cm_build=0,
+                    version_cm_releaselevel='beta',
+                    )
+    session.add(new_element)
+    session.commit()
 
 
 def main():
     engine  = create_engine(URL(**DATABASE), echo = True)
 
     # создает таблицы в БД
-    Base.metadata.create_all(engine)
+    create_db(engine)
 
     # Создаем фабрику Session для создания экземпляров
     Session = sessionmaker(bind=engine)
     # Создаем объект сессии из вышесозданной фабрики Session
     session = Session()
+
+    filling_db(session)
 
 
 if __name__ == '__main__':
@@ -93,35 +59,4 @@ if __name__ == '__main__':
 
 # --------------------------------------------------------------------------------------------------
 
-# autoincrement=True
-# primary_key=True
-# unique=True
-# nullable=False
-# default=
-# server_default =
-
-
-'''
-import models
-from app import db
-
-db.create_all()
-p = Post(title='First post', body='First post body')
-db.session.add(p)
-db.session.commit()
-'''
-
-'''
-opt_managers_bind = db.Table('opt_managers_bind',
-            db.Column('opt_user_id', INTEGER(unsigned=True), db.ForeignKey('opt_users.id'), nullable=False),
-            db.Column('opt_manager_id', INTEGER(unsigned=True), db.ForeignKey('opt_managers.id'), nullable=False)
-            )
-
-post_tags = db.Table('post_tags',
-            db.Column('post_id', db.Integer, db.ForeignKey('post.id')),
-            db.Column('tag_id', db.Integer, db.ForeignKey('tag.id'))
-            )
-
-tags = db.relationship('Tag', secondary=post_tags, backref=db.backref('posts', lazy='dynamic'))
-'''
 
---
+++ cm_db_models.py
@@ -0,0 +1,112 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from datetime import datetime
+from sqlalchemy import create_engine  #, MetaData
+from sqlalchemy.engine.url import URL
+from sqlalchemy import Table, Column, Integer, String, Text, DateTime, ForeignKey
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.sql.functions import current_timestamp
+# from sqlalchemy.dialects.mysql import INTEGER, DATETIME
+# from sqlalchemy.dialects.mysql import MEDIUMTEXT
+# UnsignedInt = db.Integer()
+# UnsignedInt = UnsignedInt.with_variant(INTEGER(unsigned=True), 'mysql')
+from configs.config import DATABASE
+
+
+Base = declarative_base()
+
+
+class BaseModel(Base):
+    __abstract__ = True
+
+    id = Column(Integer, nullable=False, unique=True, primary_key=True, autoincrement=True)
+    # created_at = Column(TIMESTAMP, nullable=False)
+    # updated_at = Column(TIMESTAMP, nullable=False)
+
+    def __repr__(self):
+        return '<{0.__class__.__name__}(id={0.id!r})>'.format(self)
+
+
+class Partitions(BaseModel):
+    __tablename__ = 'partitions'
+    name = Column(String(255), nullable=False)
+    visible = Column(Integer, nullable=False, default='1', server_default='1')
+    parent = Column(Integer, nullable=False, default='0', server_default='0')
+    description = Column(Text)
+
+
+class Elements(BaseModel):
+    __tablename__ = 'elements'
+    header = Column(String(255), nullable=False)
+    visible = Column(Integer, nullable=False, default='1', server_default='1')
+    # parent_id = Column(Integer, nullable=False, default='0', server_default='0', index=True)
+    parent_id = Column(Integer, ForeignKey('partitions.id', ondelete='CASCADE'), nullable=False, default='0', server_default='0', index=True)
+    description = Column(Text)
+    syntax = Column(String(255))
+    parameters = Column(Text)
+    example = Column(Text)
+    notes = Column(Text)
+    keywords = Column(String(255), index=True)
+    version_cm_major = Column(Integer, nullable=False, default='4', server_default='4')
+    version_cm_minor = Column(Integer, nullable=False, default='13', server_default='13')
+    version_cm_build = Column(Integer, nullable=False, default='0', server_default='0')
+    version_cm_releaselevel = Column(String(16), nullable=True, default=None, server_default=None)
+
+    def __unicode__(self):
+        return self.header
+
+    def __repr__(self):
+        return 'Element: {}'.format(self.header)
+
+
+class Users(BaseModel):
+    __tablename__ = 'users'
+    chat_id = Column(String(32), nullable=False, unique=True)
+    # created = Column(TIMESTAMP, nullable=False)
+    created = Column(DateTime, nullable=False, default=current_timestamp(), server_default='2000-01-01 00:00:00')
+    last_update = Column(DateTime, nullable=False, default=datetime(2000, 1, 1, 0, 0, 0), server_default='2000-01-01 00:00:00')
+
+
+class LogRequests(BaseModel):
+    __tablename__ = 'log_requests'
+    user_id = Column(Integer, ForeignKey('users.id', ondelete='CASCADE'), nullable=False)
+    request = Column(String(64), nullable=False)
+    rtime = Column(DateTime, nullable=False, default=current_timestamp(), server_default='2000-01-01 00:00:00')
+
+
+def create_db(engine):
+    '''создает таблиц в БД'''
+
+    Base.metadata.create_all(engine)
+
+
+if __name__ == '__main__':
+    engine = create_engine(URL(**DATABASE), echo = True)
+    create_db(engine)
+
+
+# --------------------------------------------------------------------------------------------------
+
+# autoincrement=True
+# primary_key=True
+# unique=True
+# nullable=False
+# default=
+# server_default =
+
+
+'''
+opt_managers_bind = db.Table('opt_managers_bind',
+            db.Column('opt_user_id', INTEGER(unsigned=True), db.ForeignKey('opt_users.id'), nullable=False),
+            db.Column('opt_manager_id', INTEGER(unsigned=True), db.ForeignKey('opt_managers.id'), nullable=False)
+            )
+
+post_tags = db.Table('post_tags',
+            db.Column('post_id', db.Integer, db.ForeignKey('post.id')),
+            db.Column('tag_id', db.Integer, db.ForeignKey('tag.id'))
+            )
+
+tags = db.relationship('Tag', secondary=post_tags, backref=db.backref('posts', lazy='dynamic'))
+'''
+
